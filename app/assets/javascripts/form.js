0e13b568ec4b3af862fa882eb7639bdd74fbaf46
Bharani <bharani91@gmail.com>
2016-04-20 17:40:45 +0530

    Added loader

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -2,9 +2,24 @@ $(document).on("turbolinks:load", function() {
   $("form[data-remote]")
   .on("ajax:beforeSend", function(e) {
     $(this).find("input[type='submit']").addClass("loading");
+    $("body").addClass("loading");
   })
   .on("ajax:complete", function(e) {
     $(this).find("input[type='submit']").removeClass("loading");
   });
+
+
+  $("[data-remote]")
+  .on("ajax:beforeSend", function(e) {
+    $("body").addClass("loading");
+  });
+
+  function stopLoader () {
+    $("body").removeClass("loading");
+  }
+
+  $(document).ajaxSuccess(stopLoader);
+  $(document).ajaxError(stopLoader);
+
 
 });
