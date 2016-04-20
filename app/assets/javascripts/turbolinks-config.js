6b55b72ecdec79a41d322268291522a8671235fc
Bharani <bharani91@gmail.com>
2016-04-20 17:45:08 +0530

    Minor updates

--- app/assets/javascripts/turbolinks-config.js
+++ app/assets/javascripts/turbolinks-config.js
@@ -1,9 +1,9 @@
-jQuery(document).ready(function($) {
-  $(document).on("turbolinks:request-start", function(e) {
-    $("body").addClass("loading");
-  })
-
-  $(document).on("turbolinks:render", function(e) {
-    $("body").removeClass("loading");
-  })
-});
+// jQuery(document).ready(function($) {
+//   $(document).on("turbolinks:request-start", function(e) {
+//     $("body").addClass("loading");
+//   })
+//
+//   $(document).on("turbolinks:render", function(e) {
+//     $("body").removeClass("loading");
+//   })
+// });
